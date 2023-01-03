eb3b32898445b68bec6619719c3f443de315e20c
Isaev Michael <isaevfeed@gmail.com>
2023-01-03 22:07:30 +0300

    Вынес хост и порт для SMTP в ENV

--- internal/mailer/mailer.go
+++ internal/mailer/mailer.go
@@ -10,15 +10,18 @@ import (
 )
 
 type Mailer struct {
+	Addr     string
 	Email    string
 	Password string
 }
 
 func New() *Mailer {
+	Host, _ := os.LookupEnv("SMTP_HOST")
+	Port, _ := os.LookupEnv("SMTP_PORT")
 	Email, _ := os.LookupEnv("SMTP_EMAIL")
 	Password, _ := os.LookupEnv("SMTP_PASS")
 
-	return &Mailer{Email, Password}
+	return &Mailer{Addr: fmt.Sprintf("%s:%s", Host, Port), Email: Email, Password: Password}
 }
 
 func (m *Mailer) Send(email, subject, message string) error {
@@ -28,7 +31,7 @@ func (m *Mailer) Send(email, subject, message string) error {
 	msg := strings.NewReader(fmt.Sprintf("To: %s\r\n", email) +
 		fmt.Sprintf("Subject: %s\r\n", subject) + "\r\n" + message + "\r\n")
 
-	err := smtp.SendMail("smtp.gmail.com:587", auth, m.Email, to, msg)
+	err := smtp.SendMail(m.Addr, auth, m.Email, to, msg)
 	if err != nil {
 		return err
 	}
